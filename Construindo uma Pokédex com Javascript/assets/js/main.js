8641d02c099bca9d6c3d0590bc3a86dbea79e455
Robson Valleriote Nascimento <93871640+RobsonVallerioteNascimento@users.noreply.github.com>
2025-05-02 11:06:38 -0300

    Resolução do desafio de Projeto.
    Arquivos referente a resolução do desafio de projeto - Construindo uma Pokédex com JavaScript
    Realizado na plataforma digital DIO.me

---
+++ Construindo uma Pokédex com Javascript/assets/js/main.js
@@ -0,0 +1,62 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+function clickDeatil(){
+    const pokemonListDetail = document.getElementsByClassName('pokemon')
+
+    for (let index = 0; index < pokemonListDetail.length; index++) {
+        pokemonListDetail.item(index).addEventListener('click', () => {
+            let pokemonSelect = allPokemons.filter((pokemon) => {
+                return pokemon.name == pokemonListDetail.item(index).children[1].innerHTML
+            })
+            localStorage.setItem('pokemonSelect', JSON.stringify(pokemonSelect[0]) );
+            window.location = 'pokemon'
+        })
+    }
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+        clickDeatil()
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
---
+++ Construindo uma Pokédex com Javascript/assets/js/pokemon-model.js
@@ -0,0 +1,16 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+}
+
+class Details extends Pokemon {
+    weight;
+    height;
+    species;
+    abilities;
+    eggGroup;
+}
